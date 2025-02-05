6faf432375ce397906009b1d40391c4d043fd81c
HaydenHartmann1 <156471807+HaydenHartmann1@users.noreply.github.com>
2025-02-05 08:20:07 -0600

    Homework Assignment 2
    Homework for assignment 2

---
+++ CIS122_Homework_2/Discounted_Inventory.cs
@@ -0,0 +1,96 @@
+﻿/*
+    Hayden Hartmann
+    1/24/2025
+    Buying Inventory & Discounted Inventory
+*/
+
+
+namespace Buying_Inventory_lvl10
+{
+    //class creation (Name is from original problem, Discounted Inventory is an add on)
+    public class Buying_Inventory
+    {
+        //main method
+        public static void Main(string[] args)
+        {
+            //shoping list menu
+            Console.WriteLine("Shop List");
+            Console.WriteLine("1 - Rope");
+            Console.WriteLine("2 - Torches");
+            Console.WriteLine("3 - Climbing Gear");
+            Console.WriteLine("4 - Clean Water");
+            Console.WriteLine("5 - Machete");
+            Console.WriteLine("6 - Canoe");
+            Console.WriteLine("7 - Food Supplies");
+
+            //asks user for number, assigned to num
+            Console.Write("What item would you like to see the price of (int): ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            //asks user for their name, assigned to name
+            Console.Write("What is your name?: ");
+            string name = Console.ReadLine();
+
+            //declares variables and assigns discount to 1
+            double price;
+            double discount = 1;
+
+            //if name is Hayden, then discount re-assigned to 0.5, giving half off discount
+            if (name == "Hayden")
+            {
+                discount = 0.5;
+            }
+
+            //switch case to check num against each case. Corresponds with each item
+            switch (num)
+            {
+                //if num == 1
+                case 1:
+                    //price is assigned to item cost * discount (will be 1 still if not Hayden)
+                    price = 10 * discount;
+                    //string interpolation to display
+                    Console.WriteLine($"The price for Rope is {price}g");
+                    //break statement to prevent every other case following this one to run
+                    break;
+                case 2:
+                    price = 15 * discount;
+                    Console.WriteLine($"The price for Torches is {price}g");
+                    break;
+                case 3:
+                    price = 25 * discount;
+                    Console.WriteLine($"The price for Climbing Gear is {price}g");
+                    break;
+                case 4:
+                    price = 1 * discount;
+                    Console.WriteLine($"The price for Clean Water is {price}g");
+                    break;
+                case 5:
+                    price = 20 * discount;
+                    Console.WriteLine($"The price for Machete is {price}g");
+                    break;
+                case 6:
+                    price = 200 * discount;
+                    Console.WriteLine($"The price for Canoe is {price}g");
+                    break;
+                case 7:
+                    price = 1 * discount;
+                    Console.WriteLine($"The price for Food Supplies is {price}g");
+                    break;
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ CIS122_Homework_2/The_Magic_Cannon.cs
@@ -0,0 +1,49 @@
+﻿/*
+    Hayden Hartmann
+    2/5/2025
+    The Magic Cannon
+*/
+
+//creates class
+public class TheMagicCannon
+{
+    //class main method
+    public static void Main(string[] args)
+    {
+        //loops through the numbers 1-100
+        for (int i = 1; i <= 100; i++)
+        {
+            //every 3rd and 5th crank line up
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                //changes background color before displaying the result using string interpolation
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.WriteLine($"{i}: Electric and Fire");
+            }
+            //every 5th crank
+            else if (i % 5 == 0)
+            {
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"{i}: Electric");
+            }
+            //every 3rd crank
+            else if (i % 3 == 0)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine($"{i}: Fire");
+            }
+            //all subsequent cranks
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.WriteLine($"{i}: Normal");
+            }
+        }
+
+        Console.ReadKey();
+    }
+}
+
+
+
+
